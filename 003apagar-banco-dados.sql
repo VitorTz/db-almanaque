ba846d53f8e993d57b33227f77f4120826455b89
NathanOnCodes <contatonathancf@gmail.com>
2024-12-04 15:40:27 -0300

    feat: comando drop sql

---
+++ 003apagar-banco-dados.sql
@@ -0,0 +1,28 @@
+/*
+    Exemplos do comando DROP
+    **Cuidado ao usar DROP DATABASE:** Essa operação exclui todos os dados do banco de dados.
+*/
+
+-- 1. Dropar tabela
+DROP TABLE nome_tabela;
+
+-- 2. Dropar banco de dados
+DROP DATABASE nome_banco_de_dados;
+
+-- 3. Dropar view
+DROP VIEW nome_view;
+
+-- 4. Dropar function
+DROP FUNCTION nome_funcao;
+
+-- 5. Dropar procedure
+DROP PROCEDURE nome_procedure;
+
+-- 6. Dropar índice
+DROP INDEX index_cliente_nome;
+
+-- 7. Dropar esquema
+DROP SCHEMA nome_esquema_auxiliar;
+
+-- 8. Dropar um tipo de dado
+DROP TYPE nome_tipo_de_dado;
