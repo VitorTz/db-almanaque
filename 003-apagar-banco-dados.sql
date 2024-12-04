86f664bbaa3b5a408c705d77882af368a5419ade
NathanOnCodes <contatonathancf@gmail.com>
2024-12-04 16:53:26 -0300

    fix: renomeando arquivo apagar

---
+++ 003-apagar-banco-dados.sql
@@ -0,0 +1,37 @@
+/*
+    Exemplos do comando DROP
+    **Cuidado ao usar DROP DATABASE:** Essa operação exclui todos os dados do banco de dados.
+*/
+
+-- 1. Dropar tabela
+DROP TABLE nome_tabela;
+
+-- 2. Dropar banco de dados
+DROP DATABASE nome_banco_de_dados;
+
+-- 3. Dropar view
+DROP VIEW nome_view;
+
+-- 4. Dropar function
+DROP FUNCTION nome_funcao;
+
+-- 5. Dropar procedure
+DROP PROCEDURE nome_procedure;
+
+-- 6. Dropar índice
+DROP INDEX index_cliente_nome;
+
+-- 7. Dropar esquema
+DROP SCHEMA nome_esquema_auxiliar;
+
+-- 8. Dropar um tipo de dado
+DROP TYPE nome_tipo_de_dado;
+
+-- 9. Dropar um trigger
+DROP TRIGGER trg_atualizar_estoque ON produtos; 
+
+-- 10. Dropar uma role
+DROP ROLE usuario_leitura; 
+
+-- 11. Dropar uma constraint
+ALTER TABLE pedidos DROP CONSTRAINT chk_valor_total_positivo; 
