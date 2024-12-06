8132545ae486f329b493546ca1ad484c96bb9dbc
NathanOnCodes <contatonathancf@gmail.com>
2024-12-06 18:58:42 -0300

    feat: comando DELETE 006

---
+++ 006-deletar-banco-dados.sql
@@ -0,0 +1,7 @@
+/*
+    Agora vamos deletar colunas e tabelas com o comando DELETE
+    NOTA: altamente desaconselhado para ser usado sem o comando WHERE.
+
+    Estrutura básica:
+        DELETE * FROM tabela WHERE condição;
+*/
