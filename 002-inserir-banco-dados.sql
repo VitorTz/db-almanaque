6cfdd3e97f3e30e3caf602fcf465e957966f36fb
NathanOnCodes <contatonathancf@gmail.com>
2024-12-05 22:42:04 -0300

    fix: 002 espaçamento

--- 002-inserir-banco-dados.sql
+++ 002-inserir-banco-dados.sql
@@ -6,6 +6,7 @@
 INSERT INTO carros (nome, marca, modelo, vendido, ano)
 VALUES ('Civic', 'Honda', 'Sedan', FALSE, 2019);
 
+
 -- 2. Inserir múltiplos registros com valores explícitos
 INSERT TO carros (nome, marca, modelo, vendido, ano)
 VALUES
@@ -13,9 +14,11 @@ VALUES
     ('Gol', 'Volkswagen', 'Hatchback', TRUE, 2017),
     ('Sentra', 'Nissan', 'Sedan'. FALSE, 2022);
 
+
 -- 3. Inserir dados de outra tabela
 INSERT INTO carros_vendidos (nome, marca, vendido, ano)
 SELECT nome, marca, vendido, ano FROM carros WHERE vendido = TRUE;
+
 
 -- 4. Inserir dados de um subconsulta
 INSERT TO pedidos (cliente_id, produto_id, quantidade)
@@ -24,19 +27,24 @@ FROM clientes c
 JOIN produtos p ON c.id = p.cliente_id
 WHERE p.categoria == "Eletrônicos";
 
+
 -- 5. Inserir dados gerados de uma função
 INSERT INTO logs_emprestimos (livro, usuario, data_emprestimo)
 VALUES ('O Senhor dos Anéis', 'João Silva', NOW());
 
+
 -- 6. Inserir dados a partir de um arquivo CSV (exemplo com psql)
 \copy carros (nome, marca, modelo, vendido, ano) FROM 'carros.csv' CSV HEADER;
+
 
 -- 7. Inserir dados a partir de um arquivo JSON (exemplo com psql)
 \copy carros (nome, marca, modelo, vendido, ano) FROM 'carros.json' CSV HEADER;
 
+
 -- 8. Inserir dados com valores padrão
 INSERT INTO usuarios (nome, email) 
 VALUES ('Novo usuario', 'novousuario@novoemail.com');
+
 
 -- 9. Inserir dados gerados de uma sequência
 CREATE SEQUENCE seq_pedidos;
