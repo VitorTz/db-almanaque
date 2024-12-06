5da415229021dde23328d58117b0b0ef73a3282c
NathanOnCodes <contatonathancf@gmail.com>
2024-12-05 22:32:07 -0300

    feat: comando sql JOIN v2

--- 005-join-banco-dados.sql
+++ 005-join-banco-dados.sql
@@ -4,13 +4,28 @@
 
 -- 1. Fazendo a junção entre duas tabelas.
 SELECT
-    nome_produto
+    nome_produto,
     nome_categoria
 FROM produtos p
 JOIN categorias c ON p.categoria_id = c.categoria_id;
 
 
--- 5. Fazendo a junção de todos os registros mesmo sem correspondência.
+-- 2. Fazendo a junção dando prioridade para o lado esquerdo da equação.
+SELECT
+    nome_aluno,
+    nome_materia
+FROM alunos a
+LEFT JOIN matriculas m ON a.aluno_id = m.aluno_id;
+
+-- 3. Fazendo a junção dando prioridade para o lado direito da equação.
+SELECT
+    nome_aluno,
+    nome_materia
+FROM alunos a
+RIGHT JOIN matriculas m ON a.aluno_id = m.aluno_id;
+
+
+-- 4. Fazendo a junção de todos os registros mesmo sem correspondência.
 SELECT
     nome_funcionario,
     nome_departamento
