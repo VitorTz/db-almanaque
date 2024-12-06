47cf731cf661bc260b7dd684b40c43946b8e38de
NathanOnCodes <contatonathancf@gmail.com>
2024-12-06 18:05:07 -0300

    feat: 005 comando UPDATE v2

--- 005-atualizar-banco-dados.sql
+++ 005-atualizar-banco-dados.sql
@@ -17,8 +17,25 @@ UPDATE produtos
 SET desconto = preco * 0.2, preco_parcelado = preco * 0.8
 WHERE categorias = 'Eletronicos';
 
+
 -- 4. Atualizando colunas com Subconsultas
 UPDATE produtos
 SET status = 'Enviado'
 WHERE id IN (SELECT id FROM pedidos WHERE data_envio < '2022-01-01');
 
+
+-- 5. Atualizando colunas com JOIN
+UPDATE pedidos AS p
+JOIN clientes AS c ON p.cliente_id = c.id
+SET p.status = 'Cancelado'
+WHERE c.ativo = false;
+
+
+-- 6. Atualizando com expressão
+UPDATE pedidos AS p
+SET preco = preco - (preco * 0.1)
+WHERE categoria = 'Livros';
+
+
+-- 7. Atualizando com função agregada
+UPDATE usuarios SET data_atualizacao = NOW() WHERE id = 101;
