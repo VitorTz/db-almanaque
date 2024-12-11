8f0414da782bcac9807a4f61bcafd55a0492ba77
NathanOnCodes <contatonathancf@gmail.com>
2024-12-11 13:33:18 -0300

    feat: 008 COUNT SELECT

--- 008-funcoes-agregacao-banco-dados.sql
+++ 008-funcoes-agregacao-banco-dados.sql
@@ -10,3 +10,7 @@
 -- 1. Comando AVG é usado para obter a média dos dados
 SELECT AVG(preco) FROM produtos;
 DELETE FROM produtos WHERE AVG(preco) > 30000;
+
+
+-- 2. Comando COUNT retorna a quantidade de linhas
+SELECT COUNT(id) FROM jogadores;
