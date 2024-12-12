42786c0ed8a8a46d8d1f916b44df106534b77caa
NathanOnCodes <contatonathancf@gmail.com>
2024-12-12 20:59:22 -0300

    fix: mensagem importa calcular_bonus

--- 009-procedure-banco-dados.sql
+++ 009-procedure-banco-dados.sql
@@ -6,10 +6,15 @@
     uma tarefa que executamos repetidas vezes como gerar um relatório por exemplo.
 */
 
+
+-- 1. Procedimento para calcular o bonus de um departamento com SELECT
 CREATE PROCEDURE calcular_bonus(departamento_id INTEGER)
 AS
 BEGIN
     SELECT AVG(salario) AS media_salarios
     FROM funcionarios
     WHERE departamento = departamento_id;
-END;    
+END;  
+
+-- 1.2 Chamando procedimento
+CALL calcular_bonus(2);
