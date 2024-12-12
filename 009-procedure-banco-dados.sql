47816576ec1f34a21af54088fe21dbe7a20205f1
NathanOnCodes <contatonathancf@gmail.com>
2024-12-12 20:57:06 -0300

    feat: 009 calcula_bonus procedure

--- 009-procedure-banco-dados.sql
+++ 009-procedure-banco-dados.sql
@@ -5,3 +5,11 @@
     Procedure são úteis quando queremos "automatizar" uma tarefa,
     uma tarefa que executamos repetidas vezes como gerar um relatório por exemplo.
 */
+
+CREATE PROCEDURE calcular_bonus(departamento_id INTEGER)
+AS
+BEGIN
+    SELECT AVG(salario) AS media_salarios
+    FROM funcionarios
+    WHERE departamento = departamento_id;
+END;    
