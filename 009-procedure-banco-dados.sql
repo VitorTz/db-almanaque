2fa9e62f53caab83620e0dc86f274e829416626d
NathanOnCodes <contatonathancf@gmail.com>
2024-12-12 20:47:17 -0300

    feat: 009

---
+++ 009-procedure-banco-dados.sql
@@ -0,0 +1,7 @@
+/*
+
+    Agora vamos ver como criar uma procedure.
+
+    Procedure são úteis quando queremos "automatizar" uma tarefa,
+    uma tarefa que executamos repetidas vezes como gerar um relatório por exemplo.
+*/
