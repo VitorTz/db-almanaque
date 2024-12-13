626b2ce90e84df647c861bd40cec8c5d9a9a11e0
NathanOnCodes <contatonathancf@gmail.com>
2024-12-13 18:15:18 -0300

    fix: 002 reformular query 9.

--- 002-inserir-banco-dados.sql
+++ 002-inserir-banco-dados.sql
@@ -46,7 +46,3 @@ INSERT INTO usuarios (nome, email)
 VALUES ('Novo usuario', 'novousuario@novoemail.com');
 
 
--- 9. Inserir dados gerados de uma sequência
-CREATE SEQUENCE seq_pedidos;
-INSERT INTO pedidos (id, cliente_id, produto_id, quantidade)
-VALUES (nextval('seq_pedidos'), 1, 2, 5);
