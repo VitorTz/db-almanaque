38fd18349237894b2fb4e3081cf0a5171f0cefb9
NathanOnCodes <contatonathancf@gmail.com>
2024-12-13 16:44:34 -0300

    feat: 010 GROUP BY 1.

--- 010-agrupar-banco-dados.sql
+++ 010-agrupar-banco-dados.sql
@@ -1,3 +1,9 @@
 /*
     Vamos agora ver o comando GROUP BY
+    é muito comum usar GROUP BY com funções de agregação
 */
+
+
+-- 1. Agrupar utilizando COUNT.
+SELECT categoria, COUNT(*)
+FROM produtos GROUP BY categoria;
